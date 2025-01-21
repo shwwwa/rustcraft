8dfcd9879be85eab1bb0f32b50c859bb05cecec1
eztaah <eztaah@gmail.com>
2025-01-21 08:43:47 +0100

    fix: Fix cursor not being displayed

--- client/src/ui/hud/reticle.rs
+++ client/src/ui/hud/reticle.rs
@@ -20,14 +20,17 @@ pub fn spawn_reticle(mut commands: Commands) {
         ))
         .with_children(|parent| {
             // Horizontal line (horizontal bar of the cross)
-            parent.spawn(Node {
-                position_type: PositionType::Absolute,
-                width: Val::Px(20.0),
-                height: Val::Px(2.0),
-                left: Val::Px(-10.0),
-                top: Val::Px(-1.0),
-                ..default()
-            });
+            parent.spawn((
+                Node {
+                    position_type: PositionType::Absolute,
+                    width: Val::Px(20.0),
+                    height: Val::Px(2.0),
+                    left: Val::Px(-10.0),
+                    top: Val::Px(-1.0),
+                    ..Default::default()
+                },
+                BackgroundColor(Color::WHITE),
+            ));
 
             // Vertical line (vertical bar of the cross)
             parent.spawn((
@@ -39,7 +42,7 @@ pub fn spawn_reticle(mut commands: Commands) {
                     top: Val::Px(-10.0),
                     ..Default::default()
                 },
-                // Color::WHITE.into(),
+                BackgroundColor(Color::WHITE),
             ));
         });
 }
